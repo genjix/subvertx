5e0f3091d69aa7240427e03d1ba9e097e4e73c23
genjix <fake@lol.u>
2011-11-09 18:13:06 +0000

    Intermediate step for mktx:
    mktx create -p d45c0d523ddf2f6424ba8c32533594b147cbef1d9a1c91ebc3769ea515f65129:1 -r 14DDgj2r8WQEwfTDEjJFBn3wRnHmXzgB3z:1000000 -r 14DDgj2r8WQEwfTDEjJFBn3wRnHmXzgB3z:1000000 < /tmp/privkey | mktx send

--- src/mktx.cpp
+++ src/mktx.cpp
@@ -1,14 +1,17 @@
 #include <bitcoin/address.hpp>
 #include <bitcoin/constants.hpp>
+#include <bitcoin/dialect.hpp>
 #include <bitcoin/messages.hpp>
 #include <bitcoin/kernel.hpp>
 #include <bitcoin/types.hpp>
 #include <bitcoin/transaction.hpp>
 #include <bitcoin/network/network.hpp>
+#include <bitcoin/util/assert.hpp>
 #include <bitcoin/util/elliptic_curve_key.hpp>
 #include <bitcoin/util/logger.hpp>
 #include <bitcoin/util/ripemd.hpp>
 
+#include <getopt.h>
 #include <functional>
 #include <iostream>
 #include <string>
@@ -20,19 +23,42 @@ using namespace libbitcoin::message;
 using std::placeholders::_1;
 using std::placeholders::_2;
 
+const option long_opts[] = {
+    {"previous-output", required_argument, NULL, 'p'},
+    {"recipient", required_argument, NULL, 'r'},
+    {"host", required_argument, NULL, 'H'},
+    {"port", required_argument, NULL, 'P'},
+    {"help", required_argument, NULL, 'h'},
+    {NULL, no_argument, NULL, 0}
+};
+
+const char* opt_string = "p:r:H:P:h";
+
 void display_help()
 {
-    puts("Usage: mktx [HOST[:PORT]] [OUTPUT] [ADDRESS] [AMOUNT]");
+    puts("Usage: mktx COMMAND [ARGS]...");
     puts("");
-    puts("OUTPUT consists of a transaction hash and output index");
-    puts("  125d49f6b826c564ea99345c56286de4b5126e3da38691caa4ccc68c8c8118d5:1");
-    puts("AMOUNT uses internal bitcoin values");
-    puts("  0.1 BTC = 0.1 * 10^8 = 1000000");
+    puts("Commands:");
+    puts("");
+    puts("  create\tCreate a new transaction and output the binary data");
+    puts("  send\t\tSend a transaction to the network, reading from STDIN");
+    puts("");
+    puts("Options:");
+    puts("");
+    puts(" -p, --previous-output\tPrevious output in the form OUT:INDEX");
+    puts(" -r, --recipient\tSpecify a destination ADDRESS:AMOUNT");
+    puts("\t\t\tAMOUNT uses internal bitcoin values");
+    puts("\t\t\t  0.1 BTC = 0.1 * 10^8 = 1000000");
+    puts(" -H, --host\t\tHost of bitcoin node");
+    puts(" -P, --port\t\tPort for bitcoin node");
+    puts(" -h, --help\t\tThis help text");
+    puts("");
+    puts("Please email suggestions and questions to <genjix@riseup.net>.");
 }
 
 void error_exit(const std::string& message, int status=1)
 {
-    log_error() << "balance: " << message;
+    log_error() << "mktx: " << message;
     exit(status);
 }
 
@@ -75,45 +101,37 @@ script build_output_script(const short_hash& public_key_hash)
     return result;
 }
 
-int main(int argc, char** argv)
+struct destination
 {
-    if (argc != 5)
-    {
-        display_help();
-        return 0;
-    }
-    std::vector<std::string> output_parts;
-    boost::split(output_parts, argv[2], boost::is_any_of(":"));
-    if (output_parts.size() != 2)
-        error_exit("output requires transaction hash and index");
+    std::string address;
+    uint64_t amount;
+};
 
-    std::string raw_private_key;
-    std::getline(std::cin, raw_private_key);
-    elliptic_curve_key key;
-    if (!key.set_private_key(
-            private_data(raw_private_key.begin(), raw_private_key.end())))
-        error_exit("bad private key");
+void create(std::vector<output_point> previous_outputs,
+    std::vector<destination> endpoints, const elliptic_curve_key& key)
+{
+    if (previous_outputs.size() != 1)
+        error_exit("more than 1 previous output not supported yet");
+
+    transaction tx;
+    tx.version = 1;
+    tx.locktime = 0;
 
     transaction_input input;
-    input.previous_output.hash = hash_from_pretty(output_parts[0]);
-    if (input.previous_output.hash == null_hash)
-        error_exit("malformed previous output transaction hash");
-    input.previous_output.index = 
-        boost::lexical_cast<uint32_t>(output_parts[1]);
+    input.previous_output = previous_outputs[0];
     input.sequence = 4294967295;
     data_chunk public_key = key.get_public_key();
     input.input_script.push_operation({opcode::special, public_key});
-
-    transaction_output output;
-    output.value = boost::lexical_cast<uint64_t>(argv[4]);
-    short_hash dest_pubkey_hash = address_to_short_hash(argv[3]);
-    output.output_script = build_output_script(dest_pubkey_hash);
-
-    transaction tx;
     tx.inputs.push_back(input);
-    tx.outputs.push_back(output);
-    tx.version = 1;
-    tx.locktime = 0;
+
+    for (const destination& dest: endpoints)
+    {
+        transaction_output output;
+        output.value = dest.amount;
+        short_hash dest_pubkey_hash = address_to_short_hash(dest.address);
+        output.output_script = build_output_script(dest_pubkey_hash);
+        tx.outputs.push_back(output);
+    }
 
     // Rebuild previous output script
     script script_code = 
@@ -131,13 +149,16 @@ int main(int argc, char** argv)
     input_script.push_operation({opcode::special, signature});
     input_script.push_operation({opcode::special, public_key});
 
-    std::vector<std::string> args;
-    boost::split(args, argv[1], boost::is_any_of(":"));
-    std::string hostname = args[0];
-    unsigned short port = 8333;
-    if (args.size() > 1)
-        port = boost::lexical_cast<unsigned short>(args[1]);
+    original_dialect convert_tx;
+    data_chunk raw_tx = convert_tx.to_network(tx);
+    BITCOIN_ASSERT(raw_tx ==
+        convert_tx.to_network(convert_tx.transaction_from_network(raw_tx)));
+    log_info() << std::string(raw_tx.begin(), raw_tx.end());
+}
 
+int send(const message::transaction& tx, 
+    const std::string& hostname, unsigned short port)
+{
     network_ptr net(new network_impl);
     handshake_connect(net, hostname, port, 
         std::bind(&handle_connected, _1, _2, net, tx));
@@ -148,5 +169,132 @@ int main(int argc, char** argv)
     log_info() << pretty_hex(hash_transaction(tx));
     sleep(1);
     return 0;
+}
+
+std::string read_stdin()
+{
+    std::istreambuf_iterator<char> it(std::cin);
+    std::istreambuf_iterator<char> end;
+    return std::string(it, end);
+}
+
+private_data read_private_key()
+{
+    std::string raw_private_key = read_stdin();
+    return private_data(raw_private_key.begin(), raw_private_key.end());
+}
+
+message::transaction read_transaction()
+{
+    std::string raw_tx_string = read_stdin();
+    data_chunk raw_tx(raw_tx_string.begin(), raw_tx_string.end());
+    original_dialect convert_tx;
+    return convert_tx.transaction_from_network(raw_tx);
+}
+
+int main(int argc, char** argv)
+{
+    if (argc < 2)
+    {
+        display_help();
+        return 0;
+    }
+    std::string command = argv[1];    
+    argc--;
+    argv++;
+
+    std::vector<output_point> previous_outputs;
+    std::vector<destination> endpoints;
+
+    std::string hostname = "localhost";
+    unsigned short port = 8333;
+
+    int long_index = 0;
+    int opt = getopt_long(argc, argv, opt_string, long_opts, &long_index);
+    while (opt != -1)
+    {
+        switch (opt)
+        {
+            case 'p':
+            {
+                std::vector<std::string> output_parts;
+                boost::split(output_parts, optarg, boost::is_any_of(":"));
+                if (output_parts.size() != 2)
+                    error_exit("output requires transaction hash and index");
+                output_point prevout;
+                prevout.hash = hash_from_pretty(output_parts[0]);
+                if (prevout.hash == null_hash)
+                    error_exit("malformed previous output transaction hash");
+                prevout.index = boost::lexical_cast<uint32_t>(output_parts[1]);
+                previous_outputs.push_back(prevout);
+                break;
+            }
+            
+            case 'r':
+            {
+                std::vector<std::string> dest_parts;
+                boost::split(dest_parts, optarg, boost::is_any_of(":"));
+                if (dest_parts.size() != 2)
+                    error_exit("recipient requires address and amount");
+                destination dest;
+                dest.address = dest_parts[0];
+                dest.amount = boost::lexical_cast<uint64_t>(dest_parts[1]);
+                endpoints.push_back(dest);
+                break;
+            }
+
+            case 'H':
+                hostname = optarg;
+                break;
+
+            case 'P':
+                port = boost::lexical_cast<unsigned short>(optarg);
+                break;
+
+            case 'h':
+                display_help();
+                return 0;
+
+            case 0:
+            default:
+                return -1;
+        }
+        opt = getopt_long(argc, argv, opt_string, long_opts, &long_index);
+    }
+
+    size_t number_args = argc - 2, arg_index = 2;
+    if (command == "create")
+    {
+        if (previous_outputs.empty())
+            error_exit("need at least one previous output");
+        if (endpoints.empty())
+            error_exit("need at least one recipient");
+        elliptic_curve_key key;
+        if (!key.set_private_key(read_private_key()))
+            error_exit("bad private key");
+        create(previous_outputs, endpoints, key);
+    }
+    else if (command == "send")
+    {
+        message::transaction tx;
+        try
+        {
+            tx = read_transaction();
+        }
+        catch (end_of_stream)
+        {
+            error_exit("bad transaction");
+        }
+        return send(tx, hostname, port);
+    }
+    else if (command == "help")
+    {
+        display_help();
+        return 0;
+    }
+    else
+        error_exit("not a valid command. See mktx help text.");
+    // Should never happen!
+    return 1;
 }
 
